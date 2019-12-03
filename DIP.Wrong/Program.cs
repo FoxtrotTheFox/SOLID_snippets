0f257cac07791b61d12a12acf874d35988674d46
Lorenzo Fontana <30670451+LorenzoFontana@users.noreply.github.com>
2019-12-03 16:34:28 +0100

    add wrong DIP example snippet

---
+++ DIP.Wrong/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DIP.Wrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person owner = new Person
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                EmailAddress = "tim@iamtimcorey.com",
+                PhoneNumber = "555-1212"
+            };
+
+            Project androidApp = new Project
+            {
+                ProjectName = "Take out the trash",
+                Owner = owner
+            };
+
+            androidApp.WorkOnProject(3);
+            androidApp.WorkOnProject(1.5);
+            androidApp.CompleteProject();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DIP.Wrong/Project.cs
@@ -0,0 +1,30 @@
+﻿
+namespace DIP.Wrong
+{
+    public class Project
+    {
+        public string ProjectName { get; set; }
+        public Person Owner { get; set; }
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        public void WorkOnProject(double hours)
+        {
+            HoursWorked += hours;
+            Logger log = new Logger();
+            log.Log($"Performed work on { ProjectName }");
+        }
+
+        public void CompleteProject()
+
+        {
+            IsComplete = true;
+
+            Logger log = new Logger();
+            log.Log($"Completed { ProjectName }");
+
+            Emailer emailer = new Emailer();
+            emailer.SendEmail(Owner, $"The chore { ProjectName } is complete.");
+        }
+    }
+}
